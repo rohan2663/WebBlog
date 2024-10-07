72b94f3b3f4296042f2c58f6d0e93397336c823c
Rohan Kumar <142005785+rohan2663@users.noreply.github.com>
2024-10-07 22:04:05 +0530

    Update MyBlogs.jsx

--- frontend/src/components/miniComponents/MyBlogs.jsx
+++ frontend/src/components/miniComponents/MyBlogs.jsx
@@ -10,7 +10,7 @@ const MyBlogs = () => {
   useEffect(() => {
     const fetchMyBlogs = async () => {
       const { data } = await axios.get(
-        "http://localhost:4000/api/v1/blog/myblogs",
+        "https://webblog-backend-tl6u.onrender.com/api/v1/blog/myblogs",
         { withCredentials: true }
       );
       setMyBlogs(data.blogs);
@@ -20,9 +20,12 @@ const MyBlogs = () => {
 
   const deleteBlogHandler = async (id) => {
     await axios
-      .delete(`http://localhost:4000/api/v1/blog/delete/${id}`, {
-        withCredentials: true,
-      })
+      .delete(
+        `https://webblog-backend-tl6u.onrender.com/api/v1/blog/delete/${id}`,
+        {
+          withCredentials: true,
+        }
+      )
       .then((res) => {
         toast.success(res.data.message);
         setMyBlogs((prevBlogs) => prevBlogs.filter((blog) => blog._id !== id));
