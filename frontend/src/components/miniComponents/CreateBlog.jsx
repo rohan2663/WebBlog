e4f1d451c7bb56ea653dc5691b07a8f7a6eff2b8
Rohan Kumar <142005785+rohan2663@users.noreply.github.com>
2024-10-07 22:03:44 +0530

    Update CreateBlog.jsx

--- frontend/src/components/miniComponents/CreateBlog.jsx
+++ frontend/src/components/miniComponents/CreateBlog.jsx
@@ -97,7 +97,7 @@ const CreateBlog = () => {
 
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/blog/post",
+        "https://webblog-backend-tl6u.onrender.com/api/v1/blog/post",
         formData,
         {
           withCredentials: true,
