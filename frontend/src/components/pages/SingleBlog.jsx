27a63f38fffd7120652baf9a4a6b99947a2f6514
Rohan Kumar <142005785+rohan2663@users.noreply.github.com>
2024-10-07 22:33:06 +0530

    Update SingleBlog.jsx

--- frontend/src/components/pages/SingleBlog.jsx
+++ frontend/src/components/pages/SingleBlog.jsx
@@ -11,7 +11,7 @@ const SingleBlog = () => {
     const getSingleBlog = async () => {
       try {
         const { data } = await axios.get(
-          `https://webblog-backend-tl6u.onrender.com/api/v1/blog/singleblog/${id}`,
+          `http://localhost:4000/api/v1/blog/singleblog/${id}`,
           { withCredentials: true }
         );
         setBlog(data.blog);
